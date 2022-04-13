df2f39d3350a73cf8d46c74e26b8973f79e5159a
duaaanis <103386530+duaaanis@users.noreply.github.com>
2022-04-13 12:00:58 +0500

    pa

---
+++ login_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
+
+class LoginPgae extends StatefulWidget {
+  @override
+  State<LoginPgae> createState() => _LoginPgaeState();
+}
+
+class _LoginPgaeState extends State<LoginPgae> {
+  String name = "";
+  bool changeButton = false;
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/a.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter username",
+                       labelText: "Username",
+                       ),
+                       onChanged: (value){
+                         name = value;
+                         setState(() {});
+                       },
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter pasword",
+                       labelText: "Pasword",
+                       ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    changeButton = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+               child: AnimatedContainer(
+                 duration: Duration(seconds: 1),
+                 width: changeButton ? 50 : 150,
+                 height: 50,
+                 alignment: Alignment.center,
+                 child: changeButton
+                    ?Icon(
+                      Icons.done,
+                      color: Colors.white,
+                      ) 
+                    :Text(
+                   "login",
+                   style: TextStyle(
+                     color: Colors.white,
+                     fontWeight: FontWeight.bold,
+                     fontSize: 18),
+                   ),
+                   decoration: BoxDecoration(
+                     color: Colors.deepPurple,
+                     borderRadius: 
+                     BorderRadius.circular(changeButton ? 50 : 8),
+                   ),
+                ),
+                ),
+                //ElevatedButton(
+                // child: Text("login"),
+                // style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                // onPressed: () {
+                // Navigator.pushNamed(context, MyRoutes.homeRoute);
+               // },
+               // ),
+              ],
+            
+            ),
+          ],
+        ),
+        
+        );
+  }
+}
